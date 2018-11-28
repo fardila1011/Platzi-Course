493a01908baddf5c7afee18cd12a10c20e8ad7e6
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2018-11-28 14:17:29 -0500

    Curso de .NET

---
+++ Carrera de Desarrollo de Aplicaciones con ASP .NET/ETAPA1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CorEscuela.Entidades;
+
+namespace ETAPA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi School", 2012);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad  = "Bogota";
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
